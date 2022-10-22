45847a887448900d2683a74c1543c0d072b2abfa
Nitansh Rastogi <nitansh11@Nitanshs-MacBook-Pro.local>
2022-10-22 13:05:59 +0530

    section_9

--- books2.py
+++ books2.py
@@ -1,7 +1,15 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
 from uuid import UUID
+from starlette.responses import JSONResponse
+
+
+# custom excepton class
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
+
 
 app = FastAPI()
 
@@ -15,15 +23,48 @@ class Book(BaseModel):
     rating: int = Field(gt=-1, lt=101)  # between 0 and 100 inclusive
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str
+    description: Optional[str] = Field(
+        None, title="description of the Book", max_length=100, min_length=1)
+
+
 BOOKS = []
+
+# custom exception handler
+
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request, exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={
+            "message": f"Cannot return {exception.books_to_return} negative number of books"},
+    )
+
+
+@app.post("/books/login")  # form data
+async def books_login(username: str = Form(), password: str = Form()):
+    return {"username": username, "password": password}
+
+
+@app.get("/header")
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {"random_header": random_header}
 
 
 @app.get("/")
 async def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return < 0:
+        # custom exception
+        raise NegativeNumberException(books_to_return=books_to_return)
     return BOOKS
 
 
-@app.post("/")
+# sending custom http code when book is created
+@app.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
@@ -31,6 +72,15 @@ async def create_book(book: Book):
 
 @app.get("/book/{book_id}")
 async def read_book(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+
+# send own custom response
+
+
+@app.get("/book/rating/{book_id}", response_model=BookNoRating)
+async def read_book_no_rating(book_id: UUID):
     for x in BOOKS:
         if x.id == book_id:
             return x
@@ -54,3 +104,8 @@ async def delete_book(book_id: UUID):
         if x.id == book_id:
             del BOOKS[counter-1]
             return {"message": "Book deleted"}
+    raise HTTPException(status_code=404, detail="book not found", headers={
+                        "X-Header-Error": "Nothing found at this uuid"})
+
+
+# assignment
